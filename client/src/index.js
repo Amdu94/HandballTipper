495d3ce4c73350a727cf65033bf93da3a4bb129f
Amdu94 <adam.dulai@gmail.com>
2024-03-19 19:25:48 +0100

    Create Layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+
+
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
