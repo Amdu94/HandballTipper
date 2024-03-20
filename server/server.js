d63bda2b240c015270f32e4262a2aa817e09bce2
Amdu94 <adam.dulai@gmail.com>
2024-03-20 18:53:56 +0100

    Create patch route for the guess

--- server/server.js
+++ server/server.js
@@ -15,15 +15,32 @@ if (!MONGO_URL) {
 const app = express();
 app.use(express.json());
 
-app.get("/api/matches/", async (req, res) => {
+app.get("/api/allMatches/", async (req, res) => {
     const matches = await MatchModel.find().sort({ date: "asc" });
     return res.json(matches);
+})
+app.get("/api/allMatches/:id", async (req, res) => {
+    const match = await MatchModel.findById(req.params.id);
+    return res.json(match);
 })
 
 app.get("/api/nextMatches/", async (req, res) => {
     const matches = await MatchModel.find({ date: { $gt: date }}).sort({ date: "asc" });
     return res.json(matches);
 })
+
+app.patch("/api/allMatches/:id", async (req, res, next) => {
+    try {
+        const match = await MatchModel.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: { ...req.body } },
+            { new: true }
+        );
+        return res.json(match);
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 const main = async () => {
