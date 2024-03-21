b7746749e46d0c2dfb8ef6b5acdb98ef89e89df4
Amdu94 <adam.dulai@gmail.com>
2024-03-21 23:45:24 +0100

    Update guessestable

--- client/src/Components/GuessesTable/GuessesTable.jsx
+++ client/src/Components/GuessesTable/GuessesTable.jsx
@@ -1,7 +1,6 @@
-// GuessesTable.js
-import "./GuessesTable.css";
+import React from 'react';
 
-const GuessesTable = ({ guesses, matches }) => {
+const GuessesTable = ({ guesses, matches, onGuessChange, onSaveGuess }) => {
     const getMatchById = (matchId) => {
         return matches.find((match) => match._id === matchId);
     };
@@ -14,14 +13,30 @@ const GuessesTable = ({ guesses, matches }) => {
                     <th>Match</th>
                     <th>Home Score</th>
                     <th>Away Score</th>
+                    <th>Save</th>
                 </tr>
                 </thead>
                 <tbody>
                 {guesses.map((guess, index) => (
                     <tr key={index}>
                         <td>{getMatchById(guess.match)?.home} vs {getMatchById(guess.match)?.away}</td>
-                        <td>{guess.home}</td>
-                        <td>{guess.away}</td>
+                        <td>
+                            <input
+                                type="number"
+                                value={guess.home || ''} // Ha guess.home null, akkor üres string lesz
+                                onChange={(e) => onGuessChange(index, 'home', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="number"
+                                value={guess.away || ''} // Ha guess.away null, akkor üres string lesz
+                                onChange={(e) => onGuessChange(index, 'away', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <button onClick={() => onSaveGuess(guess._id)}>Save</button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
