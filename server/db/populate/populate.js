1e0f7b0e1a073a19471f43982d8d9eedb76d0471
Amdu94 <adam.dulai@gmail.com>
2024-03-21 15:37:03 +0100

    Modified the populate logic

--- server/db/populate/populate.js
+++ server/db/populate/populate.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const matchModel = require("../match.model");
-const guessModel = require("../guess.model");
+const userModel = require("../user.model");
 const matchApi = require("../api/matchApi");
 
 const mongoUrl = process.env.MONGO_URL;
@@ -11,60 +11,43 @@ if (!mongoUrl) {
     process.exit(1);
 }
 
-
-
 const populateMatches = async () => {
-    await matchModel.deleteMany({});
 
     const fetchedMatches = await matchApi();
 
-    const matchesToDb = fetchedMatches.response.map((match) => ({
+    const existingMatches = await matchModel.find();
+
+    const matchesToAdd = fetchedMatches.response.filter(newMatch => {
+        return !existingMatches.some(existingMatch =>
+            existingMatch.home === newMatch.teams.home.name &&
+            existingMatch.away === newMatch.teams.away.name &&
+            existingMatch.date.getTime() === new Date(newMatch.date).getTime()
+        );
+    });
+
+    const matchesToDb = matchesToAdd.map(match => ({
         _id: new mongoose.Types.ObjectId(match.id),
         home: match.teams.home.name,
         away: match.teams.away.name,
         date: match.date,
         homeScore: match.scores.home,
         awayScore: match.scores.away,
-        homeGuess: null,
-        awayGuess: null,
+        guesses: []
     }));
 
     const createdMatches = await matchModel.create(...matchesToDb);
+
+    await userModel.create();
+
     console.log("Matches created");
     return createdMatches;
 };
 
-const populateGuesses = async (matches) => {
-
-    await guessModel.deleteMany({});
-
-    const guessesToDb = matches.map((match) => ({
-        _id: match._id,
-        match: `${match.home} - ${match.away}`,
-        guesses: [
-            {
-                name: "admin",
-                homeScore: 10,
-                awayScore: 20,
-            },
-            {
-                name: "admin2",
-                homeScore: 20,
-                awayScore: 10,
-            }
-        ]
-    }));
-
-    await guessModel.create(guessesToDb);
-    console.log("Guesses created");
-};
 
 const main = async () => {
     await mongoose.connect(mongoUrl);
 
-    const createdMatches = await populateMatches();
-
-    await populateGuesses(createdMatches);
+    await populateMatches();
 
     await mongoose.disconnect();
 };
