c31cc849762f623c8966bde410b5473069d691f0
Amdu94 <adam.dulai@gmail.com>
2024-03-22 19:02:41 +0100

    Refactor the calculator

--- server/services/pointsCalculator.js
+++ server/services/pointsCalculator.js
@@ -1,47 +1,53 @@
-// pointsCalculator.js
-
 const MatchModel = require("../db/models/match.model");
 const UserModel = require("../db/models/user.model");
 
+const calculatePoints = (guess, actual) => {
+    const correctResult = (guess.homeScore === actual.homeScore) && (guess.awayScore === actual.awayScore);
+    const correctGoalDifference = guess.homeScore - guess.awayScore === actual.homeScore - actual.awayScore;
+    const guessedWinner = guess.homeScore - guess.awayScore === actual.homeScore - actual.awayScore;
+
+    if (correctResult) {
+        return 5;
+    } else if (correctGoalDifference) {
+        return 3;
+    } else if (guessedWinner) {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+
 const pointsCalculator = async (matchId) => {
+    // Fetch match and throw error if not found
     const match = await MatchModel.findById(matchId);
     if (!match) {
         throw new Error("Match not found");
     }
 
-    const correctHomeScore = match.homeScore;
-    const correctAwayScore = match.awayScore;
+    const { homeScore, awayScore, guesses } = match; // Destructuring for cleaner access
 
-    for (const guess of match.guesses) {
+    for (const guess of guesses) {
         const user = await UserModel.findById(guess.user);
-
-        if (guess.homeScore === correctHomeScore && guess.awayScore === correctAwayScore) {
-            guess.points = 5;
-        } else if (guess.homeScore - guess.awayScore === correctHomeScore - correctAwayScore){
-            guess.points = 3
-        } else if ((guess.homeScore > guess.awayScore && correctHomeScore > correctAwayScore) ||
-                    (guess.homeScore < guess.awayScore && correctHomeScore < correctAwayScore) ||
-                    (guess.homeScore === guess.awayScore && correctHomeScore === correctAwayScore)){
-            guess.points = 1;
-        }
-        else {
-            guess.points = 0;
-        }
-
-        await match.save();
 
         if (!user) {
             throw new Error("User not found");
-        } else {
-            const userGuessIndex = user.guesses.findIndex(userGuess => userGuess.match.toString() === matchId);
-            if (userGuessIndex !== -1) {
-                user.guesses[userGuessIndex].points = guess.points;
-                await user.save();
-            }
+        }
+
+        guess.points = calculatePoints(guess, { homeScore, awayScore });
+
+        await match.save();
+
+        const userGuessIndex = user.guesses.findIndex((userGuess) => userGuess.match.toString() === matchId);
+        if (userGuessIndex !== -1) {
+            user.guesses[userGuessIndex].points = guess.points;
+            await user.save();
         }
     }
 };
 
 module.exports = {
-    pointsCalculator
+    pointsCalculator,
 };
+
+
