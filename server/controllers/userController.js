12b71dcc705bb0d014ed226504f716ad88aea309
Amdu94 <adam.dulai@gmail.com>
2024-03-22 18:12:57 +0100

    Create userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,6 @@
-const UserModel = require("../db/user.model");
 const userService = require("../services/userService");
 
-exports.getAllUsers = async (req, res, next) => {
+const getAllUsers = async (req, res, next) => {
     try {
         const users = await userService.getAllUsers();
         res.json(users);
@@ -10,7 +9,7 @@ exports.getAllUsers = async (req, res, next) => {
     }
 };
 
-exports.getUserById = async (req, res, next) => {
+const getUserById = async (req, res, next) => {
     try {
         const user = await userService.getUserById(req.params.id);
         res.json(user);
@@ -19,7 +18,7 @@ exports.getUserById = async (req, res, next) => {
     }
 };
 
-exports.getUserGuesses = async (req, res, next) => {
+const getUserGuesses = async (req, res, next) => {
     try {
         const guesses = await userService.getUserGuesses(req.params.id);
         res.json(guesses);
@@ -28,7 +27,7 @@ exports.getUserGuesses = async (req, res, next) => {
     }
 };
 
-exports.createUser = async (req, res, next) => {
+const createUser = async (req, res, next) => {
     try {
         const user = await userService.createUser(req.body);
         res.json(user);
@@ -37,11 +36,19 @@ exports.createUser = async (req, res, next) => {
     }
 };
 
-exports.updateUserGuess = async (req, res, next) => {
+const updateUserGuess = async (req, res, next) => {
     try {
         await userService.updateUserGuess(req.params.userId, req.params.guessId, req.body);
         res.json({ message: "Guess updated successfully" });
     } catch (err) {
         next(err);
     }
+};
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    getUserGuesses,
+    createUser,
+    updateUserGuess
 };
