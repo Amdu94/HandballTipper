3ee139d75ff319548caf981d7cf1555a4b6b8e6f
Amdu94 <adam.dulai@gmail.com>
2024-03-24 21:19:41 +0100

    Add CORS

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const matchRoutes = require("./routes/matchRoutes");
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 
+const app = express();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 const { MONGO_URL, PORT = 8080 } = process.env;
 
 if (!MONGO_URL) {
@@ -12,7 +20,7 @@ if (!MONGO_URL) {
     process.exit(1);
 }
 
-const app = express();
+
 app.use(express.json());
 
 app.use("/api/matches", matchRoutes);
