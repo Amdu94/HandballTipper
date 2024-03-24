c4f7c2fcac7bf67e7098ebe6ced55b0dce4bac41
Amdu94 <adam.dulai@gmail.com>
2024-03-24 21:20:21 +0100

    Change HomePage

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="Home">
-            <h1>Welcome to your football prediction app!</h1>
+            <h1>Welcome to your handball prediction app!</h1>
             <p>This app allows you to create a user account, view upcoming matches, and submit your predictions for their scores. You can also track your past predictions and see how well you're doing.</p>
         </div>
     );
