a2842b046f791fd0acdd2a55c966f305436626d1
Amdu94 <adam.dulai@gmail.com>
2024-03-25 15:16:08 +0100

    Use new populate logic

--- backend/db/populate/populateMatches.js
+++ backend/db/populate/populateMatches.js
@@ -1,37 +1,63 @@
-const mongoose = require("mongoose");
 const matchModel = require("../models/match.model");
 const userModel = require("../models/user.model");
 const matchApi = require("../api/matchApi");
+//const games = require("./games.json")
+const { pointsCalculator } = require("../../services/pointsCalculator")
 
- const populateMatches = async () => {
-        const fetchedMatches = await matchApi();
-        const existingMatches = await matchModel.find();
+const populateMatches = async () => {
 
-        const matchesToAdd = fetchedMatches.response.filter(newMatch => {
-            return !existingMatches.some(existingMatch =>
-                existingMatch.home === newMatch.teams.home.name &&
-                existingMatch.away === newMatch.teams.away.name &&
-                existingMatch.date.getTime() === new Date(newMatch.date).getTime()
-            );
-        });
+    const fetchedMatches = await matchApi();
+    const existingMatches = await matchModel.find();
+    const users = await userModel.find();
 
-        const matchesToDb = matchesToAdd.map(match => ({
-            home: match.teams.home.name,
-            away: match.teams.away.name,
-            date: match.date,
-            homeScore: match.scores.home,
-            awayScore: match.scores.away,
-            guesses: []
-        }));
+    // Frissítés vagy hozzáadás minden meccsre az adatbázisban
+    for (const newMatch of fetchedMatches.response) {
+        const existingMatch = existingMatches.find(match =>
+            match.home === newMatch.teams.home.name &&
+            match.away === newMatch.teams.away.name &&
+            match.date.getTime() === new Date(newMatch.date).getTime()
+        );
 
-        await matchModel.create(matchesToDb);
-        await userModel.create();
+        if (existingMatch) {
+            // Ha a meccs már létezik az adatbázisban, ellenőrizd, hogy az eredmény megváltozott-e
+            if (existingMatch.homeScore !== newMatch.scores.home || existingMatch.awayScore !== newMatch.scores.away) {
+                // Ha az eredmény megváltozott, frissítsd az adatbázist az új eredménnyel
+                existingMatch.homeScore = newMatch.scores.home;
+                existingMatch.awayScore = newMatch.scores.away;
+                await existingMatch.save();
+                console.log(`Match result updated for ${existingMatch.home} vs ${existingMatch.away}`);
+                console.log(existingMatch._id);
+                await pointsCalculator(existingMatch._id);
+            }
+        } else {
+            // Ha a meccs még nem létezik az adatbázisban, hozzáadás
+            const matchToDb = {
+                home: newMatch.teams.home.name,
+                away: newMatch.teams.away.name,
+                date: newMatch.date,
+                homeScore: newMatch.scores.home,
+                awayScore: newMatch.scores.away,
+                guesses: []
+            };
+            const newMatchDb = await matchModel.create(matchToDb);
 
-        console.log("Matches created");
-        console.log("Users created");
+            for (const user of users) {
+                user.guesses.push({
+                    match: newMatchDb._id,
+                    home: null,
+                    away: null
+                });
+                await user.save();
+            }
 
-    };
+            console.log(`New match added: ${matchToDb.home} vs ${matchToDb.away}`);
+        }
+    }
 
+    // Felhasználók létrehozása
+    //await userModel.create();
+    console.log("Users created");
+};
 
 module.exports = populateMatches;
 
