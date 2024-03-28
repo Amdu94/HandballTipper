cd943a620c170d432a69b719ead732b37884a187
Amdu94 <adam.dulai@gmail.com>
2024-03-28 10:47:33 +0100

    Refactor the user service

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,7 +1,6 @@
 const UserModel = require("../db/models/user.model");
 const MatchModel = require("../db/models/match.model");
 
-
 const getAllUsers = async () => {
     return UserModel.find().sort({ date: "asc" });
 };
@@ -18,30 +17,36 @@ const getUserGuesses = async (id) => {
 const createUser = async (userData) => {
     const { username, email, password } = userData;
     const user = await UserModel.create({ username, email, password });
-    const matches = await MatchModel.find({}, '_id');
-    const userGuesses = matches.map(match => ({ match: match._id, home: null, away: null, points: 0 })); // Hozzáadunk egy alapértelmezett pont értéket
-    user.guesses = userGuesses;
-    await user.save();
+    await initializeUserGuesses(user);
     return user;
 };
 
 const updateUserGuess = async (userId, guessId, { home, away }) => {
     const user = await UserModel.findById(userId);
-    if (!user) {
-        throw new Error("User not found");
-    }
+    if (!user) throw new Error("User not found");
+
     const guessIndex = user.guesses.findIndex(guess => guess._id.equals(guessId));
-    if (guessIndex === -1) {
-        throw new Error("Guess not found");
-    }
+    if (guessIndex === -1) throw new Error("Guess not found");
+
     user.guesses[guessIndex].home = home;
     user.guesses[guessIndex].away = away;
-    const matchId = user.guesses[guessIndex].match;
     await user.save();
+
+    await updateMatchGuess(user, userId, guessId, { home, away });
+};
+
+const initializeUserGuesses = async (user) => {
+    const matches = await MatchModel.find({}, '_id');
+    const userGuesses = matches.map(match => ({ match: match._id, home: null, away: null, points: 0 }));
+    user.guesses = userGuesses;
+    await user.save();
+};
+
+const updateMatchGuess = async (user, userId, guessId, { home, away }) => {
+    const matchId = user.guesses.find(guess => guess._id.equals(guessId)).match;
     const match = await MatchModel.findById(matchId);
-    if (!match) {
-        throw new Error("Match not found");
-    }
+    if (!match) throw new Error("Match not found");
+
     const existingGuessIndex = match.guesses.findIndex(guess => guess.user && guess.user.toString() === userId);
     if (existingGuessIndex !== -1) {
         match.guesses[existingGuessIndex].homeScore = home;
@@ -59,6 +64,3 @@ module.exports = {
     createUser,
     updateUserGuess
 };
-
-
-
