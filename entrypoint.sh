93d74c47f494adadd20ec0adab50ebf8d8060d98
Amdu94 <adam.dulai@gmail.com>
2024-04-08 12:02:20 +0200

    Update the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 # Check if the .env file exists
-if [ ! -f .env ]; then
+if [ ! -f backend/.env ]; then
   # Check if all required arguments are provided
-    if [ $# -ne 3 ]; then
+    if [ $# -ne 2 ]; then
       echo "Usage: $0 <database> <port>"
       exit 1
     fi
 
   # Create the .env file with the necessary credentials
-  echo "MONGO_URL=$1" > .env
-  echo "PORT=$2" >> .env
+  echo "MONGO_URL=$1" > backend/.env
+  echo "PORT=$2" >> backend/.env
+  # Commented out line (if needed later)
+  # 8080
 fi
