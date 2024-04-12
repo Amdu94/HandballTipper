a49046db3616dd31638ef4de17c09c55c0a4cd8f
Amdu94 <adam.dulai@gmail.com>
2024-04-12 11:55:45 +0200

    Change jest to vitest

--- backend/__tests__/pointsCalculator.test.js
+++ backend/__tests__/pointsCalculator.test.js
@@ -1,25 +1,27 @@
-const { calculatePoints } = require('../services/pointsCalculator');
+import {calculatePoints} from "../services/pointsCalculator.js";
+
+import { describe, it, expect, vi } from 'vitest';
 
 describe('Points Calculator', () => {
-    test('Should calculate points correctly for correct result', () => {
+    it('Should calculate points correctly for correct result', () => {
         const guess = { homeScore: 2, awayScore: 1 };
         const actual = { homeScore: 2, awayScore: 1 };
         expect(calculatePoints(guess, actual)).toBe(5);
     });
 
-    test('Should calculate points correctly for correct goal difference', () => {
+    it('Should calculate points correctly for correct goal difference', () => {
         const guess = { homeScore: 3, awayScore: 1 };
         const actual = { homeScore: 2, awayScore: 0 };
         expect(calculatePoints(guess, actual)).toBe(3);
     });
 
-    test('Should calculate points correctly for guessed winner', () => {
+    it('Should calculate points correctly for guessed winner', () => {
         const guess = { homeScore: 2, awayScore: 0 };
         const actual = { homeScore: 2, awayScore: 1 };
         expect(calculatePoints(guess, actual)).toBe(1);
     });
 
-    test('Should return 0 points for incorrect guess', () => {
+    it('Should return 0 points for incorrect guess', () => {
         const guess = { homeScore: 1, awayScore: 1 };
         const actual = { homeScore: 2, awayScore: 1 };
         expect(calculatePoints(guess, actual)).toBe(0);
